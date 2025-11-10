e8f936f5f6a872771a2fefb76c99a754749d5ad0
philipwur <philipwur@hotmail.com>
2025-11-10 12:33:09 +0100

    bug

--- src/workflow/OxidationAnalysis.py
+++ src/workflow/OxidationAnalysis.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 import scipy.optimize as opt
-from typing import Dict, List, Tuple, Optional, Union
+from typing import Dict, List, Tuple, Optional, Union, Set
 
 
 
